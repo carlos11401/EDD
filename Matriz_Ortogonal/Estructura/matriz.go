db756d6649912e3fdf1c4666ff14fb4de2151af0
Carlos Castro <2786954930101@ingenieria.usac.edu.gt>
2021-03-25 17:07:27 -0600

    first-first and lasta-last

--- Matriz_Ortogonal/Estructura/matriz.go
+++ Matriz_Ortogonal/Estructura/matriz.go
@@ -14,49 +14,48 @@ func New_Matriz() *Matriz{
 
 func Insertar_Matriz(matriz *Matriz,fila int, columna int, valor string) {
 
-	//Se crea un nuevo nodo con el valor ingresado
-	nuevo := New_Nodo(fila, columna, valor)
+	//Se crea un newNode nodo con el valor ingresado
+	newNode := New_Nodo(fila, columna, valor)
 	boolNewRow := false
 	boolNewCol := false
-	//********************** INSERCION EN FILAS ***********************
-
 	//primero se busca el encabezado fila
 	encFila := BuscarEncabezado(fila, matriz.EncFila)
 	encColumna := BuscarEncabezado(columna, matriz.EncColumna)
 	if encFila == nil { //significa que no se encontro
-		//se crea el encabezado con su enlace al nuevo Nodo
-		encFila = New_NodoEzdo(fila, nuevo)
+		//se crea el encabezado con su enlace al newNode Nodo
+		encFila = New_NodoEzdo(fila, newNode)
 		InsertarEncabezado(encFila, matriz.EncFila) //se insertar en el encabezado de filas
 		boolNewRow = true
 	}
-	if encColumna == nil{
-		encColumna = New_NodoEzdo(columna,nuevo)
+	if encColumna == nil {
+		encColumna = New_NodoEzdo(columna, newNode)
 		InsertarEncabezado(encColumna, matriz.EncColumna)
 		boolNewCol = true
 	}
+	// ******************** Row && newCol ********************
 	// if there's a new Col and Row is one of the matrix
-	if boolNewRow==false && boolNewCol==true{
-		if encColumna == matriz.EncColumna.Primero{
-			newNode := encColumna.Acceso
-			encFila.Acceso.Izquierda = newNode
-			newNode.Derecha = encFila.Acceso
-			encFila.Acceso = newNode
-		}else if encColumna == matriz.EncColumna.Ultimo{
+	if boolNewRow == false && boolNewCol == true {
+		if encColumna == matriz.EncColumna.Primero {
+			newNod := encColumna.Acceso
+			encFila.Acceso.Izquierda = newNod
+			newNod.Derecha = encFila.Acceso
+			encFila.Acceso = newNod
+		} else if encColumna == matriz.EncColumna.Ultimo {
 			newNode := encColumna.Acceso
 			aux := encFila.Acceso
-			for aux != nil{
-				if aux.Derecha == nil{
+			for aux != nil {
+				if aux.Derecha == nil {
 					aux.Derecha = newNode
 					newNode.Izquierda = aux
 					break
 				}
 				aux = aux.Derecha
 			}
-		}else{
+		} else {
 			newNode := encColumna.Acceso
 			aux := encFila.Acceso
-			for aux != nil{
-				if aux.Columna<newNode.Columna && newNode.Columna<aux.Derecha.Columna{
+			for aux != nil {
+				if aux.Columna < newNode.Columna && newNode.Columna < aux.Derecha.Columna {
 					// link with right node
 					aux.Derecha.Izquierda = newNode
 					newNode.Derecha = aux.Derecha
@@ -68,33 +67,34 @@ func Insertar_Matriz(matriz *Matriz,fila int, columna int, valor string) {
 				aux = aux.Derecha
 			}
 		}
-	}
+	}else
+	// ******************** newRow && Col ********************
 	// if there's a new Row and Col is one of the matrix
-	if boolNewRow==true && boolNewCol==false{
-		if encFila == matriz.EncFila.Primero{
-			newNode := encFila.Acceso
+	if boolNewRow == true && boolNewCol == false {
+		if encFila == matriz.EncFila.Primero {
+			newNod := encFila.Acceso
 			// link new node with down node
-			encColumna.Acceso.Arriba = newNode
-			newNode.Abajo = encColumna.Acceso
+			encColumna.Acceso.Arriba = newNod
+			newNod.Abajo = encColumna.Acceso
 			// access is equal at the new node
-			encColumna.Acceso = newNode
-		}else if encFila == matriz.EncFila.Ultimo{
+			encColumna.Acceso = newNod
+		} else if encFila == matriz.EncFila.Ultimo {
 			newNode := encFila.Acceso
 			aux := encColumna.Acceso
-			for aux != nil{
-				if aux.Abajo == nil{
+			for aux != nil {
+				if aux.Abajo == nil {
 					aux.Abajo = newNode
 					newNode.Arriba = aux
 					break
 				}
 				aux = aux.Abajo
 			}
-		}else{
+		} else {
 			newNode := encFila.Acceso
 			// roam col
 			aux := encColumna.Acceso
-			for aux != nil{
-				if aux.Fila<newNode.Fila && newNode.Fila<aux.Abajo.Fila{
+			for aux != nil {
+				if aux.Fila < newNode.Fila && newNode.Fila < aux.Abajo.Fila {
 					// link with down node
 					aux.Abajo.Arriba = newNode
 					newNode.Abajo = aux.Abajo
@@ -106,6 +106,58 @@ func Insertar_Matriz(matriz *Matriz,fila int, columna int, valor string) {
 				aux = aux.Abajo
 			}
 		}
+	}else
+	// ******************** Row && Col ********************
+	if boolNewCol == false && boolNewRow == false{
+		// ------- COL last and ROW last -------
+		if encColumna == matriz.EncColumna.Ultimo && encFila == matriz.EncFila.Ultimo{
+			// Roaming Row
+			actualNodeRow := encFila.Acceso
+			for actualNodeRow != nil{
+				if actualNodeRow.Derecha == nil{
+					// linking last node of ROW with newNodo
+					actualNodeRow.Derecha = newNode
+					newNode.Izquierda = actualNodeRow
+					break
+				}
+				actualNodeRow = actualNodeRow.Derecha
+			}
+			// Roaming Col
+			actualNodeCol := encColumna.Acceso
+			for actualNodeCol != nil{
+				if actualNodeCol.Abajo == nil{
+					// linking last node of Col with newNode
+					actualNodeCol.Abajo = newNode
+					newNode.Arriba = actualNodeRow
+					break
+				}
+				actualNodeCol = actualNodeCol.Abajo
+			}
+		}else
+		// ------- COL first and ROW first -------
+		if encColumna == matriz.EncColumna.Primero && encFila == matriz.EncFila.Primero{
+			// linking newNode with Node Access of ROW
+			encFila.Acceso.Izquierda = newNode
+			newNode.Derecha = encFila.Acceso
+			// linking newNode with Node Access of COL
+			encColumna.Acceso.Arriba = newNode
+			newNode.Abajo = encColumna.Acceso
+			// --- How now newNode will be the First ---
+			// access of Row is newNode
+			encFila.Acceso = newNode
+			// access of Col is newNode
+			encColumna.Acceso = newNode
+		}else
+		// ------- COL first and ROW first -------
+		if encColumna == matriz.EncColumna.Primero{
+			//
+			actualNodeCol := encColumna.Acceso
+			for actualNodeCol != nil{
+				if actualNodeCol.Fila < newNode.Fila && newNode.Fila < actualNodeCol.Abajo.Fila{
+
+				}
+				actualNodeCol = actualNodeCol.Abajo
+			}
+		}
 	}
 }
-	// ******************** INSERCION EN COLUMNAS **************************
--- Matriz_Ortogonal/main.go
+++ Matriz_Ortogonal/main.go
@@ -9,15 +9,18 @@ import (
 
 func main(){
 	matriz := Estructura.New_Matriz()
-	Estructura.Insertar_Matriz(matriz,5,2,"1")
-	Estructura.Insertar_Matriz(matriz,3,2,"2")
-	Estructura.Insertar_Matriz(matriz,6,2,"3")
-	Estructura.Insertar_Matriz(matriz,4,2,"4")
+	Estructura.Insertar_Matriz(matriz,1,2,"1")
+	Estructura.Insertar_Matriz(matriz,2,1,"2")
+	Estructura.Insertar_Matriz(matriz,1,1,"3")
+	Estructura.Insertar_Matriz(matriz,2,2,"4")
+	//Estructura.Insertar_Matriz(matriz,4,2,"4")
+	//Estructura.Insertar_Matriz(matriz,3,3,"6")
+	//Estructura.Insertar_Matriz(matriz,4,1,"5")
 	nodeRow:= matriz.EncFila.Primero
 	str := ""
 	for nodeRow != nil{
 		node := nodeRow.Acceso
-		str += "Row: "+strconv.Itoa(nodeRow.Id)+" -> "
+		str += "Row : "+strconv.Itoa(nodeRow.Id)+" :: "
 		for node != nil{
 			str += node.Valor+" <--> "
 			node = node.Derecha
@@ -28,7 +31,7 @@ func main(){
 	nodeCol:= matriz.EncColumna.Primero
 	for nodeCol != nil{
 		node := nodeCol.Acceso
-		str += "Col: "+strconv.Itoa(nodeCol.Id)+" -> "
+		str += "Col : "+strconv.Itoa(nodeCol.Id)+" :: "
 		for node != nil{
 			str += node.Valor+" <--> "
 			node = node.Abajo
